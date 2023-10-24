ffc70b73d545ecce96a087b9082c5705a5c47cda
Pamod0 <114252757+Pamod0@users.noreply.github.com>
2023-10-24 13:19:32 +0530

    Added ESLint and Husky

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
-const http = require("http");
-const fs = require("fs");
+const http = require('http')
+const fs = require('fs')
 
 const server = http.createServer((req, res) => {
-    if (req.url === "/") {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        fs.createReadStream("./index.html").pipe(res);
-
-    } else if (req.url==="/book") {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        fs.createReadStream("./form.html").pipe(res);
-    }
-    else if (req.url==="/notice") {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        fs.createReadStream("./notice.html").pipe(res);
-
-    } else if (req.method === "GET") {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        fs.createReadStream("./notice.html").pipe(res);
-    } else {
-        res.writeHead(404);
-        res.end("Page not found"); 
-    }
-});
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    fs.createReadStream('./index.html').pipe(res)
+  } else if (req.url === '/book') {
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    fs.createReadStream('./form.html').pipe(res)
+  } else if (req.url === '/notice') {
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    fs.createReadStream('./notice.html').pipe(res)
+  } else if (req.method === 'GET') {
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    fs.createReadStream('./notice.html').pipe(res)
+  } else {
+    res.writeHead(404)
+    res.end('Page not found')
+  }
+})
 
 server.listen(5000, () => {
-    console.log("Server listening on port 5000");
-});
+  console.log('Server listening on port 5000')
+})
