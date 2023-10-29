6fff7ceb2d6d4735f14f7bdcacce318974fee828
Pamod0 <114252757+Pamod0@users.noreply.github.com>
2023-10-29 11:00:36 +0530

    crud endpoint added with authentication

--- index.js
+++ index.js
@@ -1,25 +1,61 @@
-const http = require('http')
-const fs = require('fs')
+require('dotenv').config()
 
-const server = http.createServer((req, res) => {
-  if (req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/html' })
-    fs.createReadStream('./index.html').pipe(res)
-  } else if (req.url === '/book') {
-    res.writeHead(200, { 'Content-Type': 'text/html' })
-    fs.createReadStream('./form.html').pipe(res)
-  } else if (req.url === '/notice') {
-    res.writeHead(200, { 'Content-Type': 'text/html' })
-    fs.createReadStream('./notice.html').pipe(res)
-  } else if (req.method === 'GET') {
-    res.writeHead(200, { 'Content-Type': 'text/html' })
-    fs.createReadStream('./notice.html').pipe(res)
-  } else {
-    res.writeHead(404)
-    res.end('Page not found')
-  }
+const express = require('express')
+const app = express()
+
+const jwt = require('jsonwebtoken')
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.status(200).send('Welcome')
 })
 
-server.listen(5000, () => {
+// we can use 'Postman' for send a get request with body
+app.post('/login', (req, res) => {
+  const username = req.body.username
+  const user = { name: username }
+
+  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+  const response = {
+    user,
+    accessToken
+  }
+  res.json(response)
+})
+
+// plug generated token in the header using postman
+// then we can access to the protected CRUD routes
+app.get('/create', authenticateToken, (req, res) => {
+  res.status(200).send('Create')
+})
+
+app.get('/read', authenticateToken, (req, res) => {
+  res.status(200).send('Read')
+})
+
+app.get('/update', authenticateToken, (req, res) => {
+  res.status(200).send('update')
+})
+
+app.get('/delete', authenticateToken, (req, res) => {
+  res.status(200).send('delete')
+})
+
+// function for verify the token
+function authenticateToken (req, res, next) {
+  const authHeader = req.headers.authorization
+  const token = authHeader && authHeader.split(' ')[1]
+  if (token == null) return res.sendStatus(401)
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    console.log(err)
+    if (err) return res.sendStatus(403)
+    req.user = user
+    next()
+  })
+};
+
+app.listen(5000, () => {
   console.log('Server listening on port 5000')
 })
